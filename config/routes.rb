ecb7621604249777e0bd944f4b7eca8d773760a5
Cody Deckard <cjdeckard@gmail.com>
2015-06-29 18:06:08 +0000

    Learner controller and view actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'learners/new'
+
+  get 'learners/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
