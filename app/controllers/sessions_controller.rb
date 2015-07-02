b96da2ffde4c531b2cd610f9ebf635f3f201df46
Cody Deckard <cjdeckard@gmail.com>
2015-07-02 17:27:59 +0000

    Log in process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    learner = Learner.find_by_email(params[:session][:email])
+    if learner && learner.authenticate(params[:session][:password])
+      session[:user_id] = learner.id
+      redirect_to root_url
+    else
+      redirect_to login_url
+    end
   end
 
   def destroy
