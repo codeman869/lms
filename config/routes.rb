91ff960d16c7b8252805734ad68ae0ecb5ef4de9
Cody Deckard <cjdeckard@gmail.com>
2015-07-02 19:53:39 +0000

    Log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   post '/login' => 'sessions#create'
 
+  delete '/logout' => 'sessions#destroy'
+
   root 'sessions#new'
   
   # The priority is based upon order of creation: first created -> highest priority.
