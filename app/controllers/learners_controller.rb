d3760dc8d5d0f5726126a414b69adf6cf3a8801d
Cody Deckard <cjdeckard@gmail.com>
2015-07-02 13:31:50 +0000

    new and create actions for controller

--- app/controllers/learners_controller.rb
+++ app/controllers/learners_controller.rb
@@ -1,8 +1,15 @@
 class LearnersController < ApplicationController
   def new
+    @learner = Learner.new
   end
 
   def create
+    @learner = Learner.new(params)
+    if @learner.save
+      redirect_to root_url
+    else
+      redirect_to signup_url
+    end
   end
   
   private
