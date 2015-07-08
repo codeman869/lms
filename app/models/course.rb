8ac92f237f10c41818d998ffe0983ddcf6881c58
Cody Deckard <cjdeckard@gmail.com>
2015-07-08 19:21:25 +0000

    model updates

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
-    has_one :course_type, through: :instances
-    has_many :learners, through: :registrations
+    has_one :instance
+    has_one :course_type, through: :instance
+    #has_many :learners, through: :registrations
 end
--- app/models/course_type.rb
+++ app/models/course_type.rb
@@ -1,3 +1,4 @@
 class CourseType < ActiveRecord::Base
+    has_many :instances
     has_many :courses, through: :instances
 end
