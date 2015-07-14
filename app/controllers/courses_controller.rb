570d33d43990fc090ea87116d07055b35542316a
Cody Deckard <cjdeckard@gmail.com>
2015-07-14 02:27:15 +0000

    select options for assigning belongs_to to courses and create action for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,30 @@
 class CoursesController < ApplicationController
   def new
     @course = Course.new
+    @courseTypes = CourseType.all
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
   def edit
   end
+  
+  def create
+    @course = Course.new(courseParams)
+    if @course.save 
+      redirect_to "/course_types"
+    else
+      redirect_to root_url
+    end
+  end
+  
+  private
+  
+  def courseParams
+    params.require(:course).permit(:start, :end, :course_type_id)
+  end
+  
+  
 end
